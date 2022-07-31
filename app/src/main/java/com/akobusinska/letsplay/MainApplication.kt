5fb4e874fd732d65459ef951861db6ae8dfc4f70
Agata <agatapaul24@gmail.com>
2022-07-31 14:52:15 +0200

    Initial commit

---
+++ app/src/main/java/com/akobusinska/letsplay/MainApplication.kt
@@ -0,0 +1,6 @@
+package com.akobusinska.letsplay
+
+import android.app.Application
+
+class MainApplication: Application() {
+}
---
+++ app/src/main/java/com/akobusinska/letsplay/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.akobusinska.letsplay.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.akobusinska.letsplay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
